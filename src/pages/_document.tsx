b274f13623c4ebc73217926b68f5acacbc7e8815
Amar Doekhie <a.o.doekhie@gmail.com>
2025-11-20 10:00:39 +0200

    added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
+        <GoogleAnalytics gaId="G-PF1VZXCWT9" />
       </Head>        <body className="antialiased">
         <Main />
         <NextScript />
