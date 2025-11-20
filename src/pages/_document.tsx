fb0fa220efbae35c531608e647e8409128e544b0
Amar Doekhie <a.o.doekhie@gmail.com>
2025-11-20 10:12:28 +0200

    added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,34 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Document() {
+  const GA_TRACKING_ID = "G-PF1VZXCWT9";
+
   return (
     <Html lang="en">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}');
+            `,
+          }}
+        />
+
       </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
       </body>
-      <GoogleAnalytics gaId="G-PF1VZXCWT9" />
-
     </Html>
   );
 }
